55f1f683ee9bf61546853c56541bfc2f0b35f4b8
shubhrajyotidey305 <shubhrajyotidey305@gmail.com>
2022-05-19 11:09:32 +0530

    added text field

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
@@ -14,14 +13,17 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _earnings = 0;
   final List _history = [];
+  final messageTextController = TextEditingController();
+  late String? messageText;
   PusherChannelsFlutter pusher = PusherChannelsFlutter.getInstance();
 
   Future<void> _initPusher() async {
     try {
       await pusher.init(
-          apiKey: "API_KEY",
-          cluster: "CLUSTER",
+          apiKey: "503c295a8c71896beade",
+          cluster: "mt1",
           onConnectionStateChange: onConnectionStateChange);
+
       await pusher.subscribe(
           channelName: 'my_channel',
           onEvent: (event) {
@@ -54,6 +56,18 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void onTriggerEventPressed(String _money) async {
+    await pusher.trigger(PusherEvent(
+      channelName: "presence-my_channel1",
+      eventName: "my-event",
+      data: {
+        "rupee": _money,
+        "time": DateTime.now().toString().substring(0, 16),
+        "sender": "Ajay"
+      },
+    ));
+  }
+
   @override
   void initState() {
     super.initState();
@@ -63,23 +77,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Column(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.grey[300],
-                    radius: 35.0,
-                    child: const Icon(
-                      Icons.camera_alt,
-                      color: Colors.grey,
-                    ),
-                  ),
                   const SizedBox(
                     height: 10.0,
                   ),
@@ -122,28 +127,82 @@ class _HomeState extends State<Home> {
                         ],
                       ),
                     ),
-                  )
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Send Money'),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            children: [
+                              const Text('Rs:'),
+                              Expanded(
+                                child: TextField(
+                                  controller: messageTextController,
+                                  onChanged: (value) {
+                                    //Do something with the user input.
+                                    messageText = value;
+                                  },
+                                  decoration: const InputDecoration(
+                                    contentPadding: EdgeInsets.symmetric(
+                                        vertical: 10.0, horizontal: 20.0),
+                                    hintText: 'Type your amount...',
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  //Implement send functionality.
+                                  messageTextController.clear();
+                                  // function
+                                  onTriggerEventPressed(messageText!);
+                                  if (kDebugMode) {
+                                    print(messageText);
+                                  }
+                                  messageText = null;
+                                },
+                                child: const Text(
+                                  'Send',
+                                  style: TextStyle(
+                                    color: Colors.lightBlueAccent,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 20.0),
-              child: const Text('Past Transactions'),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _history.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('Rs: ${_history[index]['rupee']}'),
-                    subtitle: Text('Sender: ${_history[index]['sender']}'),
-                    trailing: Text(_history[index]['time']),
-                  );
-                },
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                child: const Text('Past Transactions'),
               ),
-              flex: 2,
-            )
-          ],
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _history.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Rs: ${_history[index]['rupee']}'),
+                      subtitle: Text('Sender: ${_history[index]['sender']}'),
+                      trailing: Text(_history[index]['time']),
+                    );
+                  },
+                ),
+                flex: 2,
+              )
+            ],
+          ),
         ),
       ),
     );
